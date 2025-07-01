8dd6d57eaac1fbb19519006f2f4cc5c0291fc4e1
Umair Maqsood <maqsoodumair03@gmail.com>
2025-07-01 11:34:20 +0500

    Initial main file with app launch. Add main file with welcome message and app() function call

---
+++ utils.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<fstream>
+#include "utils.h"
+using namespace std;
+
+int main()
+{
+
+	cout << "                                         Welcome to our Social Media app   " << endl;
+	cout << endl;
+
+	app ();
+}
